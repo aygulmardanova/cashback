f04644f45b6f36109dfc053a9d1080114560c7f6
aygulmardanova <mardanovaaigul@mail.ru>
2019-04-06 14:02:05 +0300

    Add hw cashback project

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+import {getCashBack} from "./lib.js";
+
+export function getCB() {
+
+    let purchase = document.getElementById("purchase").value;
+    let category = document.getElementById("category").value;
+
+    let cashBack;
+    switch (category) {
+        case 'usual':
+            cashBack = getCashBack(purchase, null, null);
+            break;
+        case 'increased':
+            cashBack = getCashBack(null, purchase, null);
+            break;
+        case 'special':
+            cashBack = getCashBack(null, null, purchase);
+            break;
+    }
+
+    console.log(cashBack);
+    document.getElementById("cb").innerHTML = cashBack;
+    return cashBack;
+}
+
+{
+    let purchase = 100;
+
+    console.log('usual cashback for purchase 100: ' + getCashBack(purchase, null, null));
+    console.log('increased cashback for purchase 100: ' + getCashBack(null, purchase, null));
+    console.log('special cashback for purchase 100: ' + getCashBack(null, null, purchase));
+    console.log('purchases in three categories: ' + getCashBack(purchase, purchase, purchase));
+}
---
+++ src/js/lib.js
@@ -0,0 +1,14 @@
+export function getCashBack(purchaseUsual, purchaseIncreased, purchaseSpecial) {
+
+    const usualCashBack = 0.01;
+    const increasedCashBack = 0.05;
+    const specialCashBack = 0.3;
+
+    const cb = purchaseUsual * usualCashBack +
+        purchaseIncreased * increasedCashBack +
+        purchaseSpecial * specialCashBack;
+
+    console.log('за прошедший период вы заработали: %s рублей', cb);
+
+    return cb;
+}
