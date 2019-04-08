738302b0ba475c231c958a3c56d858c278a99c86
aygulmardanova <mardanovaaigul@mail.ru>
2019-04-08 10:48:48 +0300

    code review

--- src/js/lib.js
+++ src/js/lib.js
@@ -8,7 +8,7 @@ export function getCashBack(purchaseUsual, purchaseIncreased, purchaseSpecial) {
         purchaseIncreased * increasedCashBack +
         purchaseSpecial * specialCashBack;
 
-    console.log('за прошедший период вы заработали: %s рублей', cb);
+    console.log(`за прошедший период вы заработали: ${cb} рублей`);
 
     return cb;
 }
